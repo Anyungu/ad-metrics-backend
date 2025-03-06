c59f996a2273220671ce7014cf19cdf2b73e6792
Anyungu Wanyungu <anyungucw@gmail.com>
2025-03-06 22:59:59 +0300

    randomize prometheus data

--- ad-insight-service/src/insight/insight.service.ts
+++ ad-insight-service/src/insight/insight.service.ts
@@ -1,21 +1,31 @@
+import { faker } from '@faker-js/faker/.';
 import { Injectable } from '@nestjs/common';
 import { InsightResponse } from 'src/models/graphql-types';
 
 @Injectable()
 export class InsightService {
   getAdInsights(): InsightResponse {
+    const startDate = faker.date.between({
+      from: new Date(new Date().getFullYear() - 1, 10, 1),
+      to: new Date(new Date().getFullYear(), 7, 31),
+    });
+
+    const daysToAdd = faker.number.int({ min: 1, max: 13 });
+    const endDate = new Date(startDate);
+    endDate.setDate(startDate.getDate() + daysToAdd);
+
     return {
       data: [
         {
-          impressions: '1741',
-          date_start: '2016-03-11',
-          date_stop: '2016-03-12',
+          impressions: faker.number.int({ min: 3, max: 10433 }).toString(),
+          date_start: startDate.toISOString().split('T')[0],
+          date_stop: endDate.toISOString().split('T')[0],
         },
       ],
       paging: {
         cursors: {
-          before: 'MAZDZD',
-          after: 'MAZDZD',
+          before: faker.string.alphanumeric(6),
+          after: faker.string.alphanumeric(6),
         },
       },
     };
